3513ecfb5bb4ea592cbf99ff5ce110a212569140
Rafi Kurnia Putra <rafi.putra@traveloka.com>
2018-04-16 15:36:09 +0700

    add output for example

---
+++ examples/simple/outputs.tf
@@ -0,0 +1,9 @@
+output "zone_id" {
+  value       = "${module.public_route53_hosted_zone.zone_id}"
+  description = "The hosted zone id"
+}
+
+output "name_servers" {
+  value       = "${module.public_route53_hosted_zone.name_servers}"
+  description = "A list of name servers in associated (or default) delegation set"
+}
