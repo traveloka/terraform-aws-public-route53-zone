3bfde5cbc39b1e678dadbe98948b059fe0e65bba
Rafi Kurnia Putra <rafi.kurnia.putra@gmail.com>
2018-04-17 16:37:29 +0700

    fix/conventions (#3)
    * remove description variable
    
    * add locals to generate zone's description
    
    * add NS to the output
    
    * remove description on example
    
    * change from dash to underscore
    
    * add output for example

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -2,10 +2,9 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-module "public-route53-hosted-zone" {
+module "public_route53_hosted_zone" {
   source         = "../.."
   name           = "fpr.traveloka.com"
   product_domain = "fpr"
   environment    = "production"
-  description    = "Flight Product route53 hosted zone"
 }
