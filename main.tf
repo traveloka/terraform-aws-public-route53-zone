9b4234fa23b0e7d00c66e4cfe856ee6a30ab33a2
Nur Adhi Nugroho <nur.nugroho@traveloka.com>
2020-08-26 13:06:47 +0700

    Support Terraform v0.12

--- main.tf
+++ main.tf
@@ -4,16 +4,16 @@ locals {
 }
 
 resource "aws_route53_zone" "this" {
-  name              = "${var.name}"
-  comment           = "${local.description}"
-  delegation_set_id = "${var.delegation_set_id}"
-  force_destroy     = "${var.force_destroy}"
+  name              = var.name
+  comment           = local.description
+  delegation_set_id = var.delegation_set_id
+  force_destroy     = var.force_destroy
 
   tags = {
-    "Name"          = "${var.name}"
-    "ProductDomain" = "${var.product_domain}"
-    "Environment"   = "${var.environment}"
-    "Description"   = "${local.description}"
-    "ManagedBy"     = "${local.managed_by}"
+    "Name"          = var.name
+    "ProductDomain" = var.product_domain
+    "Environment"   = var.environment
+    "Description"   = local.description
+    "ManagedBy"     = local.managed_by
   }
 }
