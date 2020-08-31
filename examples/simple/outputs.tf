965fd30bb05c56fb40051aa06910fef98dd8730a
Nur Adhi N <nur.nugroho@traveloka.com>
2020-08-31 12:44:43 +0700

    Support Terraform v0.12 (#10)
    * Support Terraform v0.12
    
    * fix outputs.tf

--- examples/simple/outputs.tf
+++ examples/simple/outputs.tf
@@ -1,9 +1,10 @@
 output "zone_id" {
-  value       = "${module.public_route53_hosted_zone.zone_id}"
+  value       = module.public_route53_hosted_zone.zone_id
   description = "The hosted zone id"
 }
 
 output "name_servers" {
-  value       = "${module.public_route53_hosted_zone.name_servers}"
+  value       = module.public_route53_hosted_zone.name_servers
   description = "A list of name servers in associated (or default) delegation set"
 }
+
--- variables.tf
+++ variables.tf
@@ -1,26 +1,26 @@
 variable "name" {
-  type        = "string"
+  type        = string
   description = "Name of the hosted zone"
 }
 
 variable "product_domain" {
-  type        = "string"
+  type        = string
   description = "Abbreviation of the product domain this Route 53 zone belongs to"
 }
 
 variable "environment" {
-  type        = "string"
+  type        = string
   description = "Environment this Route 53 zone belongs to"
 }
 
 variable "delegation_set_id" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "The delegation set ID whose NS records will be assigned the hosted zone"
 }
 
 variable "force_destroy" {
-  type        = "string"
+  type        = string
   default     = false
   description = "Whether to destroy all records inside if the hosted zone is deleted"
 }
